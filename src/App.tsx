28758e900452cc8e98006c807c9255a90dfcdf7b
Abhinandan-KP <abhirop538@gmail.com>
2025-12-18 18:58:20 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { UPIProvider } from "@/context/UPIContext";
 import Index from "./pages/Index";
+
 import ScanPage from "./pages/ScanPage";
 import PayContactsPage from "./pages/PayContactsPage";
 import SelfTransferPage from "./pages/SelfTransferPage";
